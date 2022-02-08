74a2ba8c5866ab8b58a169e13d7befab1463e950
Łukasz Kapustka <lukaszkapustka@gmail.com>
2022-02-08 20:25:01 +0100

    feat: add data ref and axios.get boilerplate

--- src/composables/useAxios.ts
+++ src/composables/useAxios.ts
@@ -1,9 +1,21 @@
 import axios from 'axios'
+import { computed, ref } from 'vue'
 
 export const useAxios = () => {
-    const testAxios = async () => {
-        return axios.get('https://api.spacexdata.com/v4/launches/latest')
-    }
+  const data = ref()
 
-    return { testAxios }
+  const get = async (url: string) => {
+    try {
+      const response = await axios.get(url)
+      data.value = await response.data
+    }
+		catch (error: unknown) {
+      console.log(error)
+    }
+  }
+
+  return {
+    data: computed(() => data.value),
+    get
+  }
 }
