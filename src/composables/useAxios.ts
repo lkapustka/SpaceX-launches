6c5599e0fc4d7c09174d8143f038c324704569c7
Łukasz Kapustka <lukaszkapustka@gmail.com>
2022-02-09 18:10:30 +0100

    feat:  add getDataById method

--- src/composables/useAxios.ts
+++ src/composables/useAxios.ts
@@ -4,18 +4,23 @@ import { computed, ref } from 'vue'
 export const useAxios = () => {
   const data = ref()
 
-  const get = async (url: string) => {
+  const get = async (route: string) => {
     try {
-      const response = await axios.get(url)
+      const response = await axios.get(`https://api.spacexdata.com/v4/${route}`)
       data.value = await response.data
     }
-		catch (error: unknown) {
+    catch (error: unknown) {
       console.log(error)
     }
   }
 
+  const getDataById = (id: string) => {
+    data.value = data.value.find((object: any) => object.id === id)
+  }
+
   return {
     data: computed(() => data.value),
-    get
+    get,
+    getDataById
   }
 }
