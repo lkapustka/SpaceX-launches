52b217f00d7fad306a2f8ddd4a095ea1c9f7f249
Łukasz Kapustka <lukaszkapustka@gmail.com>
2022-02-11 22:32:58 +0100

    feat: add home component for main view

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import HomeBox from './HomeBox.vue'
+import { useLaunches } from '../composables/useLaunches'
+
+const { data: launches, getLaunches, formatDate } = useLaunches()
+
+await getLaunches()
+
+defineEmits(['showDetail'])
+</script>
+
+
+<template>
+  <label for="name">Nazwa lotu</label>
+  <input id="name" />
+
+  <label for="date">Data lotu></label>
+  <input type="date" id="date" />
+
+  <input type="checkox" id="success-launches" />
+  <label for="success-launches">Pokaż tylko udane loty</label>
+
+  <button>Szukaj</button>
+
+  <span></span>
+
+  <home-box
+    v-for="launch in launches"
+    :flight-number="launch.flight_number"
+    :name="launch.name"
+    :date="formatDate(launch.date_local)"
+    :img-source="launch.links.flickr.original?.[0]"
+    @click="$emit('showDetail', launch.id)"
+  />
+</template>
